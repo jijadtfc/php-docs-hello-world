037c313690b9d00a46023f97664a48620006d59f
jijadtfc <134892378+jijadtfc@users.noreply.github.com>
2023-06-05 15:12:06 +0200

    Formulario

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <title>Formulario en la misma página</title>
+</head>
+<body>
+    <?php
+          if(!isset($_POST["nombre"]) && !isset($_POST["contra"])){
+    ?>
+          <form action="ejemplo.php" method="post">
+            <label for="nombre">Escribe tu nombre</label>
+            <input type="text" name="nombre" maxlength="20"/><br/>
+            <label for="contra">Escribe tu contraseña</label>
+            <input type="password" name="contra" maxlength="10"/><br/>
+            <input type="submit" value="enviar"/><br/>
+            </form>
+          <?php
+           }else{
+                  $nombre=$_POST["nombre"];
+                  $contra=$_POST["contra"];
+                  if($nombre!="Adrian"||$contra!="123456"){
+                    echo "El usuario y contraseña no son válidos \n";
+                    echo "<a href="ejemplo.php>Volver al formulario</a>";
+            }else{
+                    echo "Entrada correcta";
+            }
+    }
+    ?>
+</body>
+</html>
